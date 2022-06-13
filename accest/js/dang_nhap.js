2c019da77b05788f34507bfa1aed08c4e720b9fe
TranThiNga <ttnga1508@gmail.com>
2022-06-13 11:44:00 +0700

    hoan_chinh

--- accest/js/dang_nhap.js
+++ accest/js/dang_nhap.js
@@ -23,3 +23,55 @@ document.getElementById("eye").addEventListener("click", function () {
         hide();
     }
 }, false);
+function kt() {
+
+    var email = document.getElementById("email").value; 
+    
+    var loiemail = "";
+    if (email.length == 0) {
+        loiemail = "Email không được bỏ trống";
+        document.getElementById("loiemail").innerHTML = loiemail;
+        document.getElementById("loiemail").style.display = "";
+        document.getElementById("loiemail").style.color = "red";
+    }
+
+}
+function oc2() {
+    var email = document.getElementById("email").value;
+    var kiemtraem = /^[A-Za-z][\w$.]+@[\w]+\.\w+$/;
+    
+    if (kiemtraem.test(email)) {
+        document.getElementById("loiemail").innerHTML = "Email hợp lệ";
+        document.getElementById("loiemail").style.color = "red";
+        setTimeout(() => document.getElementById("loiemail").style.display = "none", 5000);
+    } else {
+        document.getElementById("loiemail").innerHTML = "Email không hợp lệ vd:( abcxyz@gmail.com)"
+        document.getElementById("loiemail").style.color = "red";
+        document.getElementById("loiemail").style.display = "";
+    }
+}
+// check username
+function kt() {
+    var password = document.getElementById("txt-input").value;
+    var loipas = "";
+    
+    if (password.length == "") {
+        loimsv = "Password không được bỏ trống";
+        document.getElementById('dmm').innerHTML = loipas;
+        document.getElementById("dmm").style.display = "";
+        document.getElementById("dmm").style.color = "red";
+        
+        
+    }else  if(password.length < 7)
+        {
+            loimsv = "Bạn phải nhập password lớn hơn 7 kí tự";
+        document.getElementById('dmm').innerHTML = loipas;
+        document.getElementById("dmm").style.display = "";
+        document.getElementById("dmm").style.color = "red";
+       
+        }
+    }
+
+function oc() {
+    document.getElementById("dmm").style.display = "none";
+}
